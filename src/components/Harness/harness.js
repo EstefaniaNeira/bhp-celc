135c5c058bb942b755c52f23adb72915bfd38726
Estefania Neira <pety7282@gmail.com>
2021-10-26 16:44:04 -0300

    reflejando data en pantalla 3

--- src/components/Harness/harness.js
+++ src/components/Harness/harness.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import Harness from '../../data/harness.json'
 const harness = () => {
-    const typeHarness= Harness.harnesType
-    // const [harness, setHarness] = React.useState("")
-    // setHarness(Harness.harnesType)
+    const typeHarness = Harness.harnesType
     return (
         <div className="tituloLinea">
             <h1>Selecciona tu tipo de arnés.</h1>
             <div className="stock-container">
-        {typeHarness.map((data, key) => {
-          return (
-            <div key={key}>
-              {data.name +
-              "  " +
-              data.description +
-              ", " +
-              data.sap}
+                {typeHarness.map((data, key) => {
+                    return (
+                        <div className={classes.lineRegistration} key={key}>
+                            {data.name +
+                                " , " +
+                                data.description +
+                                ", " +
+                                data.sap}
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
         </div>
     )
 }
