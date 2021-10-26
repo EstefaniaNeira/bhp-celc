71a36b173f66ca7a857c762e56fb869cac2450c1
ni.colara@hotmail.com <ni.colara@hotmail.com>
2021-10-26 11:23:15 -0300

    check-list

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,20 @@
-
 import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-
 
 import Landing from "./components/Landing/Landing";
 import Registration from "./components/Registration/Registration";
 import SOS from "./components/SOS/SOS";
-
-
+import Checklist from "./components/Checklist/Checklist";
 
 const Routes = () => {
   return (
     <>
       <Router>
-        
         <Switch>
           <Route exact path="/" component={Landing}></Route>
           <Route path="/registration" component={Registration}></Route>
+          <Route path="/Checklist" component={Checklist}></Route>
           <Route path="/SOS" component={SOS}></Route>
-      
-
         </Switch>
       </Router>
     </>
