6439020b45911eb6a5e1b988c6a6867cd77ad153
Sofia Canales Moya <sofia.sbcm@gmail.com>
2021-10-26 16:50:03 -0300

    hola

--- src/components/Harness/harness.js
+++ src/components/Harness/harness.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Harness from '../../data/harness.json'
 const harness = () => {
     console.log(Harness.harnesType)
-    const [harness, setHarness] = React.useState("")
-    setHarness(Harness.harnesType)
     return (
         <div className="tituloLinea">
             <h1>Selecciona tu tipo de arnés.</h1>
